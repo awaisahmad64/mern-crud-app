53a9c04bbeedba77e1d964fa1f0a5e717cf42b6f
Awais Ahmad <awais6412l@gmail.com>
2023-01-18 08:20:52 +0500

    display all post at home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,137 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
 
 function Home() {
   const navigate = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [updatedPost, setUpdatedPost] = useState({});
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  useEffect(() => {
+    axios
+      .get('/posts')
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err));
+  });
+  const deletePost = (id) => {
+    console.log(id);
+    axios
+      .delete(`/delete/${id}`)
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+    window.location.reload();
+  };
+  const updatePost = (post) => {
+    setUpdatedPost(post);
+    handleShow();
+  };
+  const saveUpdatedPost = () => {
+    console.log(updatedPost);
+    axios
+      .put(`/update/${updatedPost._id}`, updatedPost)
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+    handleClose();
+    // window.location.reload();
+  };
+  const changeHandle = (e) => {
+    const { name, value } = e.target;
+    setUpdatedPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
   return (
     <div style={{ maxWidth: '90%', margin: '20px auto', textAlign: 'center' }}>
-      <Button variant='outline-dark' style={{ width: '100%' }} onClick={() => navigate('create')}>
+      <Button
+        variant="outline-dark"
+        style={{ width: '100%' }}
+        onClick={() => navigate('create')}
+      >
         Create Post
       </Button>
-      <h1>Post Page</h1>
+      <div
+        style={{ maxWidth: '90%', margin: '20px auto', textAlign: 'center' }}
+      >
+        <h1 className="text-primary ">ALL POSTS HERE</h1>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Update Post</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group>
+                <Form.Control
+                  style={{ marginBottom: '1rem' }}
+                  placeholder="Title"
+                  name="title"
+                  value={updatedPost.title ? updatedPost.title : ''}
+                  onChange={changeHandle}
+                />
+                <Form.Control
+                  placeholder="description"
+                  name="description"
+                  value={updatedPost.description ? updatedPost.description : ''}
+                  onChange={changeHandle}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={saveUpdatedPost}>
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {posts ? (
+          <>
+            {posts.map((post) => {
+              return (
+                <div
+                  key={post._id}
+                  style={{
+                    border: '1px solid lightgray',
+                    borderRadius: '8px',
+                    marginBottom: '1rem',
+                    padding: '1rem',
+                  }}
+                >
+                  <h4>{post.title}</h4>
+                  <p>{post.description}</p>
+                  <div
+                    style={{ display: 'flex', justifyContent: 'space-between' }}
+                  >
+                    <Button
+                      variant="outline-info"
+                      onClick={() => updatePost(post)}
+                    >
+                      Update
+                    </Button>
+                    <Button
+                      variant="outline-danger"
+                      onClick={() => deletePost(post._id)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   );
 }
